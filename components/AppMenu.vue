e89b24444441ef007ae08e63084a6e1a9aa71364
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-06-11 20:33:42 +0800

    chore: app menu style

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
 const isCollapsed = ref(false)
-const cssClass = computed(() => {
-  return isCollapsed.value
-    ? "w-16 hover:w-56"
-    : "w-56"
-})
 
 function toggleCollapsed() {
   isCollapsed.value = !isCollapsed.value
-  console.log(isCollapsed.value)
-  console.log(cssClass.value)
 }
 
 const links = [{
@@ -22,7 +15,7 @@ const links = [{
 <template>
   <div
     class="h-full flex flex-col justify-between overflow-x-hidden border-gray-300 dark:border-gray-600 border-r p-2"
-    :class="cssClass"
+    :class="`${isCollapsed ? 'w-16' : 'w-56'}`"
   >
     <UVerticalNavigation
       :links="links"
