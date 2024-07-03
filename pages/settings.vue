8c86dd778023485245ecbf89f2a4ef1e1190ba55
NanamiNakano <64841155+NanamiNakano@users.noreply.github.com>
2024-07-04 00:05:06 +0800

    feat: General settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,13 +1,115 @@
 <script lang="ts" setup>
-import type { AdminSystemSettings } from "@nanaminakano/pfsdk"
+import type { PendingAdminSystemSettings } from "@nanaminakano/pfsdk"
+import { z } from "zod"
 import RFormGroup from "~/components/RFormGroup.vue"
-import { abbreviatedSha } from "~build/git"
+import type { FormSubmitEvent } from "#ui/types"
+import { tag, abbreviatedSha } from "~build/git"
 
-const siteSetting = ref({} as AdminSystemSettings)
+const pfClient = usePfClient()
+const toast = useToast()
+const siteSetting = useSiteSettingStore()
+
 const items = computed(() => [{
   slot: "general",
   label: "General",
 }])
+
+const generalSchema = z.object({
+  site_name: z.string(),
+  script_mirror: z.string(),
+  pkg_mirror: z.string(),
+  register: z.string(),
+  register_invite: z.string(),
+  register_recaptcha: z.string(),
+  recaptcha_public: z.string(),
+  recaptcha_private: z.string(),
+  invite_reward: z.string(),
+  invite_reward_percentage: z.string(),
+  invite_reward_for_new_user: z.string(),
+  system_url: z.string(),
+  license: z.string(),
+  secure_key: z.string(),
+  suspend_days: z.string(),
+})
+
+async function fetchGeneral() {
+  await pfClient.admin.system.getSettings().then((rps) => {
+    if (rps.Ok) {
+      generalState.value = rps.Data! as PendingAdminSystemSettings
+    }
+    else {
+      toast.add({
+        title: "Unable to load system settings",
+        description: rps.Msg,
+        color: "red",
+        actions: [{ label: "Reload", click: fetchGeneral }],
+      })
+    }
+  })
+}
+
+type GeneralSchema = z.output<typeof generalSchema>
+
+const generalState = ref({} as PendingAdminSystemSettings)
+
+const submittingGeneral = ref(false)
+
+async function onSubmitGeneral(event: FormSubmitEvent<GeneralSchema>) {
+  submittingGeneral.value = true
+  await pfClient.admin.system.modifySettings(event.data).then(async (rps) => {
+    if (rps.Ok) {
+      toast.add({ title: "Settings saved!" })
+      await fetchGeneral()
+      siteSetting.fetch()
+    }
+    else {
+      toast.add({ title: "Unable to save settings", description: rps.Msg, color: "red" })
+    }
+  })
+  submittingGeneral.value = false
+}
+
+const register = computed({
+  get() {
+    return generalState.value.register === "true"
+  },
+  set(value: boolean) {
+    generalState.value.register = String(value)
+  },
+})
+
+const registerInvite = computed({
+  get() {
+    return generalState.value.register_invite === "true"
+  },
+  set(value: boolean) {
+    generalState.value.register_invite = String(value)
+  },
+})
+
+const registerRecaptcha = computed({
+  get() {
+    return generalState.value.register_recaptcha === "true"
+  },
+  set(value: boolean) {
+    generalState.value.register_recaptcha = String(value)
+  },
+})
+
+const inviteRewardOptions = [{
+  value: "0",
+  label: "Off",
+}, {
+  value: "1",
+  label: "First order only",
+}, {
+  value: "2",
+  label: "Repeat",
+}]
+
+onMounted(async () => {
+  await fetchGeneral()
+})
 </script>
 
 <template>
@@ -18,15 +120,120 @@ const items = computed(() => [{
       orientation="vertical"
     >
       <template #general>
-        <div class="flex flex-col gap-4">
+        <UForm
+          :schema="generalSchema"
+          :state="generalState"
+          class="space-y-4"
+          @submit="onSubmitGeneral"
+        >
           <RFormGroup title="Backend Version">
-            {{ siteSetting.version }}
+            {{ siteSetting.siteSetting.version }}
           </RFormGroup>
           <RFormGroup title="Frontend Version">
-            NEXT-{{ abbreviatedSha }}
+            NEXT-{{ tag ? tag : "dev" }}-{{ abbreviatedSha }}
           </RFormGroup>
           <UDivider />
-        </div>
+          <RFormGroup
+            name="site_name"
+            title="Site name"
+          >
+            <UInput v-model="generalState.site_name" />
+          </RFormGroup>
+          <RFormGroup
+            name="system_url"
+            title="System URL"
+          >
+            <UInput v-model="generalState.system_url" />
+          </RFormGroup>
+          <RFormGroup
+            name="license"
+            title="License"
+          >
+            <UInput v-model="generalState.license" />
+          </RFormGroup>
+          <RFormGroup
+            name="secure_key"
+            title="Secure Key"
+          >
+            <UInput v-model="generalState.secure_key" />
+          </RFormGroup>
+          <RFormGroup
+            name="script_mirror"
+            title="Script Mirror"
+          >
+            <UInput v-model="generalState.script_mirror" />
+          </RFormGroup>
+          <RFormGroup
+            name="pkg_mirror"
+            title="Package Mirror"
+          >
+            <UInput v-model="generalState.pkg_mirror" />
+          </RFormGroup>
+          <RFormGroup
+            name="suspend_days"
+            title="Suspend Days"
+          >
+            <UInput
+              v-model="generalState.suspend_days"
+            />
+          </RFormGroup>
+          <RFormGroup
+            name="register"
+            title="Register"
+          >
+            <UToggle v-model="register" />
+          </RFormGroup>
+          <RFormGroup
+            name="register_invite"
+            title="Register Invite"
+          >
+            <UToggle v-model="registerInvite" />
+          </RFormGroup>
+          <RFormGroup
+            name="register_recaptcha"
+            title="Register reCAPTCHA"
+          >
+            <UToggle v-model="registerRecaptcha" />
+          </RFormGroup>
+          <RFormGroup
+            name="recaptcha_public"
+            title="reCAPTCHA Public Key"
+          >
+            <UTextarea v-model="generalState.recaptcha_public" />
+          </RFormGroup>
+          <RFormGroup
+            name="recaptcha_private"
+            title="reCAPTCHA Private Key"
+          >
+            <UTextarea v-model="generalState.recaptcha_private" />
+          </RFormGroup>
+          <RFormGroup
+            name="invite_reward"
+            title="Invite Reward"
+          >
+            <URadioGroup
+              v-model="generalState.invite_reward"
+              :options="inviteRewardOptions"
+            />
+          </RFormGroup>
+          <RFormGroup
+            name="invite_reward_percentage"
+            title="Invite Reward Percentage"
+          >
+            <UInput v-model="generalState.invite_reward_percentage" />
+          </RFormGroup>
+          <RFormGroup
+            name="invite_reward_for_new_user"
+            title="Invite Reward For New User"
+          >
+            <UInput v-model="generalState.invite_reward_for_new_user" />
+          </RFormGroup>
+          <UButton
+            :loading="submittingGeneral"
+            label="Save"
+            type="submit"
+          />
+        </UForm>
       </template>
     </UTabs>
   </div>
